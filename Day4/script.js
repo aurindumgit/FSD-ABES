8c3f26c1190e5b04f42070766481fc09c0bb5b9b
Aurindum Bose <111241030+aurindumgit@users.noreply.github.com>
2024-11-05 14:16:32 +0530

    Add files via upload

---
+++ Day4/script.js
@@ -0,0 +1,19 @@
+// let x = confirm('Are you sure you want to log out?');
+// alert(x);
+
+// let v = prompt("Enter your name","Rahul");
+// alert (v);
+
+// let disp = () => {
+//     console.log("Welcome");
+// };
+
+// disp();
+
+// let say=()=>console.log("hello");
+// say()
+
+let sum=(a,b)=>a+b;
+
+let res=sum(12,23);
+console.log(res);
